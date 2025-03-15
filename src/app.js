8d14649f032cf41b92f6a240f0898075d1992bdb
Bdiaz18 <bdiaz.r18@gmail.com>
2025-03-15 11:26:25 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+  let pronoun = ['the', 'our', 'their', 'its'];
+  let adj = ['great', 'big', 'almighty', 'astronomical'];
+  let noun = ['jogger', 'racoon', 'protector', 'commander'];
+  let dom = [".com", ".co", ".gov", ".es", ".net"]
+
+  for (let i in pronoun) {
+    for (let j in adj) {
+      for (let k in noun) {
+        for (let l in dom) {
+          console.log(pronoun[i] + adj[j] + noun[k] + dom[l])
+        }
+      }
+    }
+  }
+}
